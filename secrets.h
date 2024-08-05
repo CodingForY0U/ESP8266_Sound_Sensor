4f84e56aaa316a8a939985f2ea179766042c49c0
Heman <167865014+CodingForY0U@users.noreply.github.com>
2024-08-05 17:31:20 +0800

    Create secrets.h
    To be used in conjunction with main code

---
+++ secrets.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char* ssid = "your_SSID_here";
+const char* pass = "your_password_here";
+unsigned long myChannelNumber = 123456; // Replace with your actual channel number
+const char* myWriteAPIKey = "your_API_key_here";
+
+#endif
